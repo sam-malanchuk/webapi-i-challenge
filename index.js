0fe0fb354c4c8aa3e8b4db9b85a50f178585b2bb
sam-malanchuk <malanchuksam@gmail.com>
2019-08-11 11:32:02 -0400

    created PUT api to update a user by ID

--- index.js
+++ index.js
@@ -78,7 +78,37 @@ server.delete('/api/users/:id', (req, res) => {
             res.status(500).json({
                 error: "The user could not be removed." 
             });
-        })
+        });
+});
+
+// PUT update()
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    if(changes.name && changes.bio) {
+        db.update(id, changes)
+            .then(updated => {
+                if (updated) {
+                    res.status(200).json({
+                        message: "The user with the specified ID has been updated."
+                    });
+                } else {
+                    res.status(404).json({
+                        message: "The user with the specified ID does not exist."
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: "The user information could not be modified"
+                });
+            });
+    } else {
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user"
+        });
+    }
 });
 
 server.listen(4000, () => {
